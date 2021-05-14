4491b6f8ef221148866b922927872a83a055cd94
shamanth-keni <81251149+shamanth-keni@users.noreply.github.com>
2021-05-14 21:41:28 +0530

    Update time.java

--- Project 2/time.java
+++ Project 2/time.java
@@ -3,17 +3,12 @@ import java.util.Scanner;
 public class time {
   public static void main(String[] args) {
     Scanner min = new Scanner(System.in);
-    // Prompt user for number of minutes
     System.out.println("Enter the number of minutes:");
     int minutes = min.nextInt();
     min.close();
-
-     // Number of minutes in a year
     int year = minutes / 525600;
     int day = minutes / 1440;
     int remainingMinutes = day % 525600;
-
-
     System.out.println(minutes + " minutes is approximately " + year + " years and "  +  remainingMinutes + " days ");
     }
 
